f97da9941fc4f60dd7e1bebff810d0abb8c7bb5f
Gabriel Maia Gondim <41156120+gammag4@users.noreply.github.com>
2026-03-20 23:17:33 -0300

    Chain build commands

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
-conda activate $1
-mkdir -p build && cd build
-cmake -DPython3_EXECUTABLE=$(which python) ..
-make
-cd ..
-./build/tensor_renderer $2
+#!/usr/bin/bash
+
+cwd=$(pwd)
+
+conda activate $1 \
+&& mkdir -p build && cd build \
+&& cmake -DPython3_EXECUTABLE=$(which python) .. \
+&& make \
+&& cd .. \
+&& ./build/tensor_renderer $2 $3
+
+cd "$cwd"
+
